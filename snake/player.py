c0cb1b347f3815706222701f51cd608ca33fe60b
omung789 <omung.bhasin.21@ucl.ac.uk>
2021-10-14 18:33:10 +0100

    added WASD support

--- snake/player.py
+++ snake/player.py
@@ -4,9 +4,13 @@ class Player:
         self.choice = "UP"
 
         self.master.bind("<Left>", lambda x: self.set_choice("LEFT"))
+        self.master.bind("<a>", lambda x: self.set_choice("LEFT"))
         self.master.bind("<Right>", lambda x: self.set_choice("RIGHT"))
+        self.master.bind("<d>", lambda x: self.set_choice("RIGHT"))
         self.master.bind("<Up>", lambda x: self.set_choice("UP"))
+        self.master.bind("<w>", lambda x: self.set_choice("UP"))
         self.master.bind("<Down>", lambda x: self.set_choice("DOWN"))
+        self.master.bind("<s>", lambda x: self.set_choice("DOWN"))
 
     def make_move(self, grid, head):
         return self.choice
